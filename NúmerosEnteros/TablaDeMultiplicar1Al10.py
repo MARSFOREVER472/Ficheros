a59cd2a0d528adfe43feda3fcfcfe050535e446e
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-10-30 10:53:21 -0300

    Se crearon tablas de multiplicar del 1 al 10.

---
+++ NúmerosEnteros/TablaDeMultiplicar1Al10.py
@@ -0,0 +1,10 @@
+# ESCRIBIR UNA FUNCIÓN QUE PIDA UN NÚMERO ENTERO ENTRE EL 1 Y EL 10 Y LO GUARDE EN UN FICHERO CON EL NOMBRE tabla-n.txt LA TABLA DE MULTIPLICAR DE ESE NÚMERO, EN DONDE "n" ES EL NÚMERO INTRODUCIDO POR LA CONSOLA.
+
+n = int(input('INTRODUCE UN NÚMERO ENTERO ENTRE EL 1 Y EL 10: '))
+nombre_fichero = 'tabla-' + str(n) + '.txt'
+f = open(nombre_fichero, 'w')
+
+for i in range(1, 11):
+    f.write(str(n) + ' x ' + str(i) + ' = ' + str(n * i) + '\n')
+
+f.close()
